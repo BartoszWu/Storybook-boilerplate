490264c8fdf4b23604c3f21939556b11fd4c956a
Bartosz Wiśniewski <bartosz.wisniewski@adverity.com>
2022-12-28 10:29:51 +0100

    Push clean version

--- src/stories/Button.tsx
+++ src/stories/Button.tsx
@@ -1,54 +1,37 @@
-import * as React from 'react';
-import './button.css';
+import * as React from "react";
+import "./styles/button.css";
 
 interface ButtonProps {
-  /**
-   * Is this the principal call to action on the page?
-   */
+  backgroundColor?: any;
   primary?: boolean;
-  /**
-   * What background color to use
-   */
-  backgroundColor?: string;
-  /**
-   * Button contents
-   */
-  label: string;
-  /**
-   * Optional click handler
-   */
+  size?: "small" | "medium" | "large";
+  label?: string;
   onClick?: () => void;
-
-  children?: React.ReactNode;
 }
-
-type Props = ButtonProps & React.AllHTMLAttributes<HTMLElement>
-
-export const ButtonBase = React.forwardRef<HTMLButtonElement, Props>(
-  ({ children, ...props }, ref) => {
-
-    return <div>{children}</div>
-  })
 
 /**
  * Primary UI component for user interaction
  */
-export const Button = React.forwardRef<HTMLButtonElement, React.ComponentProps<typeof ButtonBase>>(({
-  primary = false,
-  size = 'medium',
+export const Button = ({
+  primary,
   backgroundColor,
+  size,
   label,
   ...props
-}, ref) => {
-  const mode = primary ? 'storybook-button--primary' : 'storybook-button--secondary';
+}: ButtonProps) => {
+  const mode = primary
+    ? "storybook-button--primary"
+    : "storybook-button--secondary";
   return (
     <button
-      type={"button" as any}
-      className={['storybook-button', `storybook-button--${size}`, mode].join(' ')}
-      style={{ backgroundColor }}
+      type="button"
+      className={["storybook-button", `storybook-button--${size}`, mode].join(
+        " "
+      )}
+      style={backgroundColor && { backgroundColor }}
       {...props}
     >
       {label}
     </button>
   );
-});
+};
